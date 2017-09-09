26a5ee439df9dad5c5e299a67686d2184fdfebe4
Carissa <carissa@edgren.nu>
2017-09-09 12:17:57 +0200

    Adds boxed to order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909081558) do
+ActiveRecord::Schema.define(version: 20170909101637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20170909081558) do
     t.text "allergies"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "boxes"
   end
 
 end
