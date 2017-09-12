3590a6ad1d58d1cb9f285a015b3416788a9ff1ed
j0sv <svensson.jonas@gmail.com>
2017-09-12 09:43:00 +0200

    Add rspec test order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_db_column :id }
+  it { is_expected.to have_db_column :name }
+  it { is_expected.to have_db_column :description }
+  it { is_expected.to have_db_column :price }
+  it { is_expected.to have_db_column :order_date }
+  it { is_expected.to have_db_column :delivery_date }
+  it { is_expected.to have_db_column :address }
+  it { is_expected.to have_db_column :phone }
+  it { is_expected.to have_db_column :allergies }
+  it { is_expected.to have_db_column :created_at }
+  it { is_expected.to have_db_column :updated_at }
+  it { is_expected.to have_db_column :boxes }
+  it { is_expected.to have_db_column :status }
+  it { is_expected.to have_db_column :email }
+  it { is_expected.to have_db_column :id }
+  it { is_expected.to have_db_column :id }
 end
